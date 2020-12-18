4878cb6e31a6f8a6eef3506f3db2fb3746b39c12
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-18 14:34:46 +0100

    project lombok

--- src/main/java/javer/restapi/dto/PersonDto.java
+++ src/main/java/javer/restapi/dto/PersonDto.java
@@ -1,12 +1,17 @@
 package javer.restapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDto {
 
     private Integer id;
     private String name;
     private String lastName;
     private String pesel;
-
-
 
 }
