f7075c9006a0f458b9fb7d0ea3acb3323917d257
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-18 17:37:47 +0100

    add person controller

--- src/main/java/javer/restapi/controller/PersonController.java
+++ src/main/java/javer/restapi/controller/PersonController.java
@@ -1,4 +1,19 @@
 package javer.restapi.controller;
 
+import javer.restapi.dto.PersonDto;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/person")
 public class PersonController {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonDto getPerson() {
+        return null;
+    }
+
 }
