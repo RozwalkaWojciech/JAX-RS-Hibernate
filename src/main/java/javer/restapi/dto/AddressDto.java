e4f451b5447d3890e7bf6a099f68cab11ea7ba30
Wojciech <wojciechrozwalka@gmail.com>
2020-12-19 22:48:25 +0100

    add AddressDto fields

--- src/main/java/javer/restapi/dto/AddressDto.java
+++ src/main/java/javer/restapi/dto/AddressDto.java
@@ -1,4 +1,20 @@
 package javer.restapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor //check isNeed
+@NoArgsConstructor //check isNeed
 public class AddressDto {
+
+    private Integer id;
+    private String state;
+    private String zip;
+    private String city;
+    private String street;
+    private Integer houseNumber;
+    private Integer apartmentNumber;
+
 }
